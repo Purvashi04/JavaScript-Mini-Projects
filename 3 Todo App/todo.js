3ea46eee417a4be3cf71b41df2e256527fc3ff05
Purvashi Kupuli <155556671+Purvashi04@users.noreply.github.com>
2025-08-04 15:22:20 +0530

    Update todo.js

--- 3 Todo App/todo.js
+++ 3 Todo App/todo.js
@@ -1,11 +1,11 @@
 let todoList = [
   {
     item: 'Buy Milk',
-    dueDate: '4/10/2023'
+    dueDate: '4/08/2025'
   },
   {
     item: 'Go to College',
-    dueDate: '4/10/2023'
+    dueDate: '4/08/2025'
   }
 ];
 
@@ -35,4 +35,5 @@ function displayItems() {
     `;
   }
   containerElement.innerHTML = newHtml;
+
 }
